91716d3d41c1b7fd750f9d4b1d3ad949f800cf6d
Kamil Chruścinski <kamil.chruscinski@gmail.com>
2019-04-04 00:55:12 +0200

    Added transition script and dumbledore

---
+++ Assets/Scripts/ScaleSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleSetter : MonoBehaviour
+{
+    private static int SCALE_ID = Shader.PropertyToID("_Scale");
+
+    private SpriteRenderer spriteRenderer { get { if (_sr == null) _sr = GetComponent<SpriteRenderer>(); return _sr; } }
+    private SpriteRenderer _sr;
+
+    private new Camera camera { get { if (_camera == null) _camera = Camera.main; return _camera; } }
+    private Camera _camera;
+
+    private Vector4 data;
+
+    [SerializeField] private float scale = 0.2f;
+
+    void Update()
+    {
+        Vector4 viwport = camera.ScreenToViewportPoint(Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f));
+        spriteRenderer.sharedMaterial.SetVector(SCALE_ID, viwport * scale);
+    }
+}
